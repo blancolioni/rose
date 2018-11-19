1f117d67702d9b5c343527be225bd4216dd19b36
Fraser Wilson <blancolioni@gmail.com>
2018-11-19 13:38:12 +0100

    Rose.Kernel.Invocation: removed unused page fault handler

--- src/kernel/rose-kernel-invocation.ads
+++ src/kernel/rose-kernel-invocation.ads
@@ -7,14 +7,4 @@ package Rose.Kernel.Invocation is
 
    pragma Export (C, Invoke_Capability, "invoke_capability");
 
-   procedure Handle_Page_Fault
-     (Address              : Rose.Addresses.Virtual_Address;
-      User_Mode            : Boolean;
-      Protection_Violation : Boolean;
-      Write_Attempt        : Boolean;
-      Execution_Attempt    : Boolean);
-
-   function Page_Fault_Count
-     return Natural;
-
 end Rose.Kernel.Invocation;
