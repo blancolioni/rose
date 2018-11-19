7daab3d46a8dcaf3cd5bec425ad1f54352153daa
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2018-11-19 13:38:12 +0100

    Rose.Kernel.Invocation: removed unused page fault handler

--- src/kernel/rose-kernel-invocation.adb
+++ src/kernel/rose-kernel-invocation.adb
@@ -19,72 +19,6 @@ with Rose.Invocation.Trace;
 
 package body Rose.Kernel.Invocation is
 
-   Current_Page_Fault_Count : Natural := 0;
-
-   -----------------------
-   -- Handle_Page_Fault --
-   -----------------------
-
-   procedure Handle_Page_Fault
-     (Address              : Rose.Addresses.Virtual_Address;
-      User_Mode            : Boolean;
-      Protection_Violation : Boolean;
-      Write_Attempt        : Boolean;
-      Execution_Attempt    : Boolean)
-   is
-      Virtual_Page : constant Rose.Addresses.Virtual_Page_Address :=
-                       Rose.Addresses.Virtual_Address_To_Page (Address);
-   begin
-
-      if False then
-         Rose.Boot.Console.Put
-           ("page-fault: ");
-         Rose.Boot.Console.Put (Rose.Words.Word (Address));
-         Rose.Boot.Console.Put (" ");
-         Rose.Boot.Console.Put
-           (Rose.Kernel.Processes.Current_ESP
-              (Rose.Kernel.Processes.Current_Process_Id));
-         Rose.Boot.Console.Put (" ");
-         Rose.Boot.Console.Put (if User_Mode then 'u' else 's');
-         Rose.Boot.Console.Put (if Protection_Violation then 'p' else 'r');
-         Rose.Boot.Console.Put (if Write_Attempt then 'w' else '-');
-         Rose.Boot.Console.Put (if Execution_Attempt then 'x' else '-');
-         Rose.Boot.Console.New_Line;
-         Rose.Kernel.Processes.Debug.Report_Process
-           (Rose.Kernel.Processes.Current_Process_Id, False);
-
-      end if;
-
-      Rose.Kernel.Validation.Validate;
-
-      Current_Page_Fault_Count :=
-        Current_Page_Fault_Count + 1;
-
-      if not Rose.Kernel.Processes.Have_Process_Handlers then
-         Rose.Kernel.Panic.Panic
-           (Message => "page fault but no handler installed",
-            Addr    => Address);
-      end if;
-
-      if not User_Mode then
-         Rose.Kernel.Panic.Panic
-           (Message => "page fault in supervisor mode",
-            Addr    => Address);
-      end if;
-
-      Rose.Kernel.Processes.Handle_Page_Fault
-        (Virtual_Page      => Virtual_Page,
-         Is_Mapped         => Protection_Violation,
-         Read_Attempt      =>
-            not Write_Attempt and then not Execution_Attempt,
-         Write_Attempt     => Write_Attempt,
-         Execution_Attempt => Execution_Attempt);
-
-      Rose.Kernel.Processes.Queue.Choose_Process;
-
-      Rose.Kernel.Validation.Validate;
-   end Handle_Page_Fault;
-
    -----------------------
    -- Invoke_Capability --
    -----------------------
@@ -158,16 +92,5 @@ package body Rose.Kernel.Invocation is
       Rose.Kernel.Validation.Validate;
 
    end Invoke_Capability;
-
-   ----------------------
-   -- Page_Fault_Count --
-   ----------------------
-
-   function Page_Fault_Count
-     return Natural
-   is
-   begin
-      return Current_Page_Fault_Count;
-   end Page_Fault_Count;
 
 end Rose.Kernel.Invocation;
