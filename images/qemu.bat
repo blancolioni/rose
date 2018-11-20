0584eecbc0697122f84f2478e6880c156fe08d41
Fraser Wilson <blancolioni@gmail.com>
2018-11-20 09:53:33 +0100

    qemu script: use rose.iso for the cdrom

--- images/qemu.bat
+++ images/qemu.bat
@@ -1,1 +1,3 @@
-"C:\Program Files\qemu\qemu-system-i386.exe" -drive file=rose-boot-floppy.img,index=0,if=floppy,format=raw -m 8 -serial file:../logs/qemu-port-1.txt -drive file=rose-hd0.img,index=0,index=0,format=raw
+"C:\Program Files\qemu\qemu-system-i386.exe" -drive file=rose-boot-floppy.img,index=0,if=floppy,format=raw -m 8 -serial file:../logs/qemu-port-1.txt -drive file=rose-hd0.img,index=0,index=0,format=raw -cdrom rose.iso
+rem below is the interrupt trace call
+rem "C:\Program Files\qemu\qemu-system-i386.exe" -drive file=rose-boot-floppy.img,index=0,if=floppy,format=raw -m 8 -serial file:../logs/qemu-port-1.txt -drive file=rose-hd0.img,index=0,index=0,format=raw -d int > trace.txt 2>&1
