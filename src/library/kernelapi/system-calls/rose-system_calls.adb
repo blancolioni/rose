69e43a4ca10d89b0d60b83e7d95b4fd79ad932cd
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2018-11-22 19:56:25 +0100

    Rose.System_Calls: implemented missing subprograms

--- src/library/kernelapi/system-calls/rose-system_calls.adb
+++ src/library/kernelapi/system-calls/rose-system_calls.adb
@@ -1,7 +1,57 @@
+with Rose.Addresses;
+
 package body Rose.System_Calls is
 
    procedure Invoke_Capability_Wrapper
      (Item : aliased in out Rose.Invocation.Invocation_Record);
+
+   ---------------
+   -- Copy_Text --
+   ---------------
+
+   procedure Copy_Text
+     (Params   : Rose.Invocation.Invocation_Record;
+      To       : out String;
+      Last     : out Natural)
+   is
+   begin
+      if not Params.Control.Flags (Rose.Invocation.Send_Buffer) then
+         Last := 0;
+         return;
+      end if;
+
+      declare
+         Text : String (1 .. Natural (Params.Buffer_Length));
+         pragma Import (Ada, Text);
+         for Text'Address use System'To_Address (Params.Buffer_Address);
+      begin
+         Last := 0;
+         for Ch of Text loop
+            exit when Last >= To'Last;
+            Last := Last + 1;
+            To (Last) := Ch;
+         end loop;
+      end;
+   end Copy_Text;
+
+   ---------------------
+   -- Initialize_Send --
+   ---------------------
+
+   procedure Initialize_Send
+     (Params : in out Rose.Invocation.Invocation_Record;
+      Cap    : Rose.Capabilities.Capability)
+   is
+      use Rose.Invocation;
+   begin
+      Params := (others => <>);
+      Params.Control.Flags (Send) := True;
+      Params.Control.Flags (Block) := True;
+      Params.Control.Flags (Create_Reply_Cap) := True;
+      Params.Control.Last_Sent_Word := Parameter_Word_Index'Last;
+      Params.Control.Last_Sent_Cap := Capability_Index'Last;
+      Params.Cap := Cap;
+   end Initialize_Send;
 
    ------------
    -- Invoke --
@@ -153,5 +203,97 @@ package body Rose.System_Calls is
    begin
       Invoke (Cap, (1 => Reply), In_Words, In_Caps, Out_Words, Out_Caps);
    end Invoke_Reply;
+
+   --------------------
+   -- Receive_Buffer --
+   --------------------
+
+   procedure Receive_Buffer
+     (Params   : in out Rose.Invocation.Invocation_Record)
+   is
+   begin
+      Params.Control.Flags (Rose.Invocation.Recv_Buffer) := True;
+   end Receive_Buffer;
+
+   ------------------
+   -- Receive_Caps --
+   ------------------
+
+   procedure Receive_Caps
+     (Params : in out Rose.Invocation.Invocation_Record;
+      Count  : Natural)
+   is
+   begin
+      Params.Control.Flags (Rose.Invocation.Recv_Caps) := Count > 0;
+      if Count > 0 then
+         Params.Control.Last_Recv_Cap :=
+           Rose.Invocation.Capability_Index (Count - 1);
+      end if;
+   end Receive_Caps;
+
+   -------------------
+   -- Receive_Words --
+   -------------------
+
+   procedure Receive_Words
+     (Params : in out Rose.Invocation.Invocation_Record;
+      Count  : Natural)
+   is
+   begin
+      Params.Control.Flags (Rose.Invocation.Recv_Words) := Count > 0;
+      if Count > 0 then
+         Params.Control.Last_Recv_Word :=
+           Rose.Invocation.Parameter_Word_Index (Count - 1);
+      end if;
+   end Receive_Words;
+
+   -----------------
+   -- Send_Buffer --
+   -----------------
+
+   procedure Send_Buffer
+     (Params   : in out Rose.Invocation.Invocation_Record;
+      Bytes    : Natural;
+      Buffer   : System.Address;
+      Writable : Boolean)
+   is
+   begin
+      Params.Control.Flags (Rose.Invocation.Send_Buffer) := True;
+      Params.Control.Flags (Rose.Invocation.Writable_Buffer) := Writable;
+      Params.Buffer_Address :=
+        Rose.Addresses.To_Virtual_Address (Buffer);
+      Params.Buffer_Length := Rose.Words.Word (Bytes);
+   end Send_Buffer;
+
+   --------------
+   -- Send_Cap --
+   --------------
+
+   procedure Send_Cap
+     (Params : in out Rose.Invocation.Invocation_Record;
+      Cap    : Rose.Capabilities.Capability)
+   is
+      use type Rose.Invocation.Capability_Index;
+   begin
+      Params.Control.Last_Sent_Cap :=
+        Params.Control.Last_Sent_Cap + 1;
+      Params.Caps (Params.Control.Last_Sent_Cap) := Cap;
+   end Send_Cap;
+
+   ---------------
+   -- Send_Word --
+   ---------------
+
+   procedure Send_Word
+     (Params : in out Rose.Invocation.Invocation_Record;
+      Value  : Rose.Words.Word)
+   is
+      use type Rose.Invocation.Parameter_Word_Index;
+   begin
+      Params.Control.Flags (Rose.Invocation.Send_Words) := True;
+      Params.Control.Last_Sent_Word :=
+        Params.Control.Last_Sent_Word + 1;
+      Params.Data (Params.Control.Last_Sent_Word) := Value;
+   end Send_Word;
 
 end Rose.System_Calls;
