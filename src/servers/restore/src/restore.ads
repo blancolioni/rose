8d9a1cb9ab856d7ee63e925bba59e31634934537
Fraser Wilson <blancolioni@gmail.com>
2018-11-23 16:52:27 +0100

    restore: added cap 6, which should be a file system interface root directory endpoint. restore: new package Restore.Installer (just reports the directory contents for now)

--- src/servers/restore/src/restore.ads
+++ src/servers/restore/src/restore.ads
@@ -4,10 +4,11 @@ package Restore is
 
    pragma Pure (Restore);
 
-   Create_Endpoint_Cap         : constant Rose.Capabilities.Capability := 1;
-   Console_Cap                 : constant Rose.Capabilities.Capability := 2;
-   Block_Device_Parameters_Cap : constant Rose.Capabilities.Capability := 3;
-   Block_Device_Read_Cap       : constant Rose.Capabilities.Capability := 4;
-   Block_Device_Write_Cap      : constant Rose.Capabilities.Capability := 5;
+   Create_Endpoint_Cap           : constant Rose.Capabilities.Capability := 1;
+   Console_Cap                   : constant Rose.Capabilities.Capability := 2;
+   Block_Device_Parameters_Cap   : constant Rose.Capabilities.Capability := 3;
+   Block_Device_Read_Cap         : constant Rose.Capabilities.Capability := 4;
+   Block_Device_Write_Cap        : constant Rose.Capabilities.Capability := 5;
+   Install_File_System           : constant Rose.Capabilities.Capability := 6;
 
 end Restore;
