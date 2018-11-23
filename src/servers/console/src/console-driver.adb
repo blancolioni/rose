ffb4e170f6819c9125c28f7c98759f68871925de
Fraser Wilson <blancolioni@gmail.com>
2018-11-23 16:47:33 +0100

    Console.Driver: removed dead code

--- src/servers/console/src/console-driver.adb
+++ src/servers/console/src/console-driver.adb
@@ -9,13 +9,7 @@ begin
 
    Console.Calls.Invoke_Memory_Cap;
 
-   if False then
-      for I in 1 .. 16 loop
-         Console.IO.New_Line;
-      end loop;
-   else
-      Console.IO.Clear;
-   end if;
+   Console.IO.Clear;
    Console.IO.Put_Line (Rose.Version.Full_Name);
    Console.Server.Create_Console_Server;
    Console.Server.Start_Server;
