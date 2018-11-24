9d4f50f0e72ac9f55d6bafb6aab8cd801756df42
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2018-11-24 21:15:00 +0100

    restore: removed partition initialisation

--- src/servers/restore/src/restore-installer.adb
+++ src/servers/restore/src/restore-installer.adb
@@ -11,10 +11,9 @@ package body Restore.Installer is
    is
       use Rose.Interfaces.Directory.Client;
    begin
-      Rose.Console_IO.Put_Line ("ls -l /cd");
       for I in 1 .. Directory_Entry_Count (From) loop
          declare
-            Name : String (1 .. 16) := (others => ' ');
+            Name : String (1 .. 32) := (others => ' ');
             Last : Natural;
          begin
             Directory_Entry_Name (From, I, Name, Last);
