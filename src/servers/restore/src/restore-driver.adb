94f066cd073d8ab90ba6d3f84b632f0cb31bb148
Fraser Wilson <blancolioni@gmail.com>
2018-11-24 21:15:00 +0100

    restore: removed partition initialisation

--- src/servers/restore/src/restore-driver.adb
+++ src/servers/restore/src/restore-driver.adb
@@ -1,13 +1,10 @@
 with Rose.Devices.Block.Client;
 with Rose.Devices.GPT;
-with Rose.Devices.Partitions;
 
 with Rose.Interfaces.File_System.Client;
 with Rose.Interfaces.Directory.Client;
 
 with Rose.Console_IO;
-
-with Rose.Words;
 
 with Rose.Invocation;
 with Rose.System_Calls.Server;
@@ -20,103 +17,27 @@ procedure Restore.Driver is
 begin
    Rose.Console_IO.Open (Console_Cap);
 
+   Rose.Console_IO.Put_Line ("restore: opening swap device");
+
    Rose.Devices.Block.Client.Open
      (Device         => Device,
       Parameters_Cap => Block_Device_Parameters_Cap,
       Read_Cap       => Block_Device_Read_Cap,
       Write_Cap      => Block_Device_Write_Cap);
 
-   declare
-      use Rose.Words;
-      Block_Size   : constant Rose.Devices.Block.Block_Size_Type :=
-                       Get_Block_Size (Device);
-      Block_Count  : constant Rose.Devices.Block.Block_Address_Type :=
-                       Get_Block_Count (Device);
-      Storage_Size : constant Rose.Devices.Block.Device_Size_Type :=
-                       Rose.Devices.Block.To_Device_Size
-                         (Block_Size, Block_Count);
-   begin
-      Rose.Console_IO.Put ("device: size ");
-      Rose.Console_IO.Put (Natural (Word_64 (Storage_Size) / 2 ** 20));
-      Rose.Console_IO.Put ("M");
-      Rose.Console_IO.New_Line;
+   Rose.Console_IO.Put_Line ("restore: looking for system image");
 
-      if Rose.Devices.GPT.Has_GPT (Device) then
-         Rose.Console_IO.Put_Line ("Found GPT partition header");
-      else
+   if not Rose.Devices.GPT.Has_GPT (Device) then
+      Rose.Console_IO.Put_Line ("restore: GPT partition header not found");
+      return;
+   end if;
 
-         Rose.Console_IO.Put_Line ("Intitialising device");
+   if Rose.Devices.GPT.Partition_Count (Device) = 0 then
+      Rose.Console_IO.Put_Line ("restore: no partitions found on device");
+      return;
+   end if;
 
-         Rose.Devices.GPT.Initialize_GPT (Device);
-      end if;
-
-      if not Rose.Devices.GPT.Has_GPT (Device) then
-         Rose.Console_IO.Put_Line ("Device initialization failed");
-         return;
-      end if;
-
-      if Rose.Devices.GPT.Partition_Count (Device) > 0 then
-         Rose.Console_IO.Put_Line ("Scanning partitions");
-         Rose.Devices.GPT.Report_Partition_Table (Device);
---
---           for I in 1 .. Rose.Devices.GPT.Partition_Count (Device) loop
---              declare
---                 First_Block         : Rose.Devices.Block.Block_Address_Type;
---                 Last_Block          : Rose.Devices.Block.Block_Address_Type;
---                 Partition_Type_Low  : Rose.Words.Word_64;
---                 Partition_Type_High : Rose.Words.Word_64;
---                 Partition_Flags     : Rose.Words.Word_64;
---                 Partition_Name      : String (1 .. 40);
---                 Partition_Name_Last : Natural;
---              begin
---                 Rose.Devices.GPT.Get_Partition_Details
---                   (Device, I,
---                    First_Block, Last_Block,
---                    Partition_Type_Low, Partition_Type_High,
---                    Partition_Flags,
---                    Partition_Name, Partition_Name_Last);
---
---
---           end loop;
-
-      else
-         declare
-            use Rose.Devices.Block;
-            use Rose.Devices.Partitions;
-            Available_Blocks : constant Block_Address_Type :=
-                                 Block_Count - 4;
-            Partition_Size   : constant Block_Address_Type :=
-                                 Available_Blocks / 4;
-            Current_Start    : Block_Address_Type := 3;
-            Name             : String  := "rose-swap-#";
-         begin
-
-            Rose.Console_IO.Put_Line ("creating partitions");
-
-            for I in 1 .. 4 loop
-               Name (Name'Last) :=
-                 Character'Val (48 + I);
-
-               Rose.Devices.GPT.Add_Partition
-                 (Block_Device        => Device,
-                  First_Block         => Current_Start,
-                  Last_Block          => Current_Start + Partition_Size - 1,
-                  Partition_Type_Low  => Swap_Id_Low,
-                  Partition_Type_High => Swap_Id_High,
-                  Partition_Flags     => 0,
-                  Partition_Name      => Name);
-
-               Current_Start := Current_Start + Partition_Size;
-
-            end loop;
-
-            Rose.Devices.GPT.Flush (Device);
-
-         end;
-
-         Rose.Devices.GPT.Report_Partition_Table (Device);
-      end if;
-   end;
+   Rose.Console_IO.Put_Line ("Scanning partitions");
 
    declare
       use Rose.Interfaces.File_System.Client;
--- src/servers/restore/src/restore.ads
+++ src/servers/restore/src/restore.ads
@@ -9,6 +9,7 @@ package Restore is
    Block_Device_Parameters_Cap   : constant Rose.Capabilities.Capability := 3;
    Block_Device_Read_Cap         : constant Rose.Capabilities.Capability := 4;
    Block_Device_Write_Cap        : constant Rose.Capabilities.Capability := 5;
-   Install_File_System           : constant Rose.Capabilities.Capability := 6;
+   Add_Storage_Cap               : constant Rose.Capabilities.Capability := 6;
+   Install_File_System           : constant Rose.Capabilities.Capability := 7;
 
 end Restore;
