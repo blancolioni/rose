c7cc24ec525a9c8659a6baff94675c45ffad5289
Fraser Wilson <blancolioni@gmail.com>
2018-12-03 10:09:02 +0100

    restore: added delete cap

--- src/servers/restore/src/restore.ads
+++ src/servers/restore/src/restore.ads
@@ -5,13 +5,14 @@ package Restore is
    pragma Pure (Restore);
 
    Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 1;
-   Console_Cap         : constant Rose.Capabilities.Capability := 2;
-   Active_Swap_Cap     : constant Rose.Capabilities.Capability := 3;
-   Inactive_Swap_Cap   : constant Rose.Capabilities.Capability := 4;
-   Log_Cap             : constant Rose.Capabilities.Capability := 5;
-   Add_Storage_Cap     : constant Rose.Capabilities.Capability := 6;
-   Write_System_Image  : constant Rose.Capabilities.Capability := 7;
-   Install_Media_Cap   : constant Rose.Capabilities.Capability := 8;
-   Install_Exec_Cap    : constant Rose.Capabilities.Capability := 9;
+   Delete_Cap          : constant Rose.Capabilities.Capability := 2;
+   Console_Cap         : constant Rose.Capabilities.Capability := 3;
+   Active_Swap_Cap     : constant Rose.Capabilities.Capability := 4;
+   Inactive_Swap_Cap   : constant Rose.Capabilities.Capability := 5;
+   Log_Cap             : constant Rose.Capabilities.Capability := 6;
+   Add_Storage_Cap     : constant Rose.Capabilities.Capability := 7;
+   Write_System_Image  : constant Rose.Capabilities.Capability := 8;
+   Install_Media_Cap   : constant Rose.Capabilities.Capability := 9;
+   Install_Exec_Cap    : constant Rose.Capabilities.Capability := 10;
 
 end Restore;
