0bb32ac5a534f2a3bed429e43a6a50552889d93b
Fraser Wilson <blancolioni@gmail.com>
2018-12-05 12:41:59 +0100

    store: Store: start with three caps: create-endpoint, delete-endpoint, and console-write.

--- src/servers/store/src/store.ads
+++ src/servers/store/src/store.ads
@@ -7,10 +7,7 @@ package Store is
    pragma Pure (Store);
 
    Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 1;
-   Console_Cap         : constant Rose.Capabilities.Capability := 2;
-
-   Device_Parameters_Cap : constant Rose.Capabilities.Capability := 3;
-   Device_Read_Cap       : constant Rose.Capabilities.Capability := 4;
-   Device_Write_Cap      : constant Rose.Capabilities.Capability := 5;
+   Delete_Endpoint_Cap : constant Rose.Capabilities.Capability := 2;
+   Console_Cap         : constant Rose.Capabilities.Capability := 3;
 
 end Store;
