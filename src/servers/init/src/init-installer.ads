00a6ef31b8e1f9adfba81f6e4f072c500a97fa55
Fraser Wilson <blancolioni@gmail.com>
2018-12-05 15:24:08 +0100

    init: new package Init.Installer

---
+++ src/servers/init/src/init-installer.ads
@@ -0,0 +1,9 @@
+package Init.Installer is
+
+   procedure Launch_With_Caps
+     (Create_Cap    : Rose.Capabilities.Capability;
+      Launch_Cap    : Rose.Capabilities.Capability;
+      Cap_Stream    : Rose.Capabilities.Capability;
+      Binary_Stream : Rose.Capabilities.Capability);
+
+end Init.Installer;
