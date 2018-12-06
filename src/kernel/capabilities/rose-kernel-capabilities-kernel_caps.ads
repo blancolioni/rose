d79fde038f0a5fd2a20a1adf062cb6ddf34f42d0
Fraser Wilson <blancolioni@gmail.com>
2018-12-06 16:05:26 +0100

    kernel: kernel-cap: added create-process endpoint

--- src/kernel/capabilities/rose-kernel-capabilities-kernel_caps.ads
+++ src/kernel/capabilities/rose-kernel-capabilities-kernel_caps.ads
@@ -3,6 +3,7 @@ package Rose.Kernel.Capabilities.Kernel_Caps is
    Enable_Paging_Endpoint     : constant Rose.Objects.Endpoint_Index := 1;
    Start_Checkpoint_Endpoint  : constant Rose.Objects.Endpoint_Index := 2;
    Start_Image_Write_Endpoint : constant Rose.Objects.Endpoint_Index := 3;
+   Create_Process_Endpoint    : constant Rose.Objects.Endpoint_Index := 4;
 
    procedure Handle
      (Cap    : Rose.Capabilities.Layout.Capability_Layout;
