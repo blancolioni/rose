c1a9faade1922ccaf7eea010b8a9abe84bf0cca1
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2018-12-09 12:25:49 +0100

    Rose.Server: added support for instanced interfaces

--- src/library/kernelapi/system-calls/rose-server.adb
+++ src/library/kernelapi/system-calls/rose-server.adb
@@ -6,6 +6,62 @@ with Rose.System_Calls.Server;
 
 package body Rose.Server is
 
+   ----------------------
+   -- Get_Instance_Cap --
+   ----------------------
+
+   function Get_Instance_Cap
+     (Instance   : Server_Instance;
+      Endpoint   : Rose.Objects.Endpoint_Id;
+      Identifier : Rose.Objects.Capability_Identifier)
+      return Rose.Capabilities.Capability
+   is
+   begin
+      for I in 1 .. Instance.Instance_Count loop
+         declare
+            use Rose.Objects;
+            Item : Instanced_Handler_Record renames Instance.Instances (I);
+         begin
+            if Item.Endpoint = Endpoint
+              and then Item.Identifier = Identifier
+            then
+               return Item.Capability;
+            end if;
+         end;
+      end loop;
+      return Rose.Capabilities.Null_Capability;
+   end Get_Instance_Cap;
+
+   ------------------
+   -- Has_Instance --
+   ------------------
+
+   function Has_Instance
+     (Instance   : Server_Instance;
+      Endpoint   : Rose.Objects.Endpoint_Id;
+      Identifier : Rose.Objects.Capability_Identifier)
+      return Boolean
+   is
+   begin
+      for I in 1 .. Instance.Instance_Count loop
+         declare
+            use Rose.Objects;
+            Item : Instanced_Handler_Record renames Instance.Instances (I);
+         begin
+            if Item.Endpoint = Endpoint
+              and then Item.Identifier = Identifier
+            then
+               return True;
+            end if;
+         end;
+      end loop;
+      return False;
+   end Has_Instance;
+
+   ---------------------
+   -- Receive_Message --
+   ---------------------
+
    procedure Receive_Message
      (Context : in out Server_Context)
    is
@@ -75,6 +131,28 @@ package body Rose.Server is
           (Endpoint => Endpoint,
            Handler  => Handler);
    end Register_Handler;
+
+   ----------------------
+   -- Set_Instance_Cap --
+   ----------------------
+
+   procedure Set_Instance_Cap
+     (Instance   : in out Server_Instance;
+      Endpoint   : Rose.Objects.Endpoint_Id;
+      Identifier : Rose.Objects.Capability_Identifier;
+      Cap        : Rose.Capabilities.Capability)
+   is
+   begin
+      if Instance.Instance_Count < Max_Endpoint_Instances then
+         Instance.Instance_Count :=
+           Instance.Instance_Count + 1;
+         Instance.Instances (Instance.Instance_Count) :=
+           Instanced_Handler_Record'
+             (Endpoint   => Endpoint,
+              Identifier => Identifier,
+              Capability => Cap);
+      end if;
+   end Set_Instance_Cap;
 
    ------------------
    -- Start_Server --
