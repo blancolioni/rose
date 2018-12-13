d627d9cd2827364d94acb5a71f53242cec50021c
Fraser Wilson <blancolioni@gmail.com>
2018-12-13 16:06:37 +0100

    init: using interface caps for storage (mostly)

--- src/servers/init/src/init-installer.ads
+++ src/servers/init/src/init-installer.ads
@@ -5,6 +5,7 @@ package Init.Installer is
    procedure Install_Exec_Library
      (Create_Cap    : Rose.Capabilities.Capability;
       Storage_Cap   : Rose.Capabilities.Capability;
+      Reserve_Cap   : Rose.Capabilities.Capability;
       Launch_Cap    : Rose.Capabilities.Capability;
       Cap_Stream    : Rose.Capabilities.Capability;
       Binary_Stream : Rose.Capabilities.Capability;
