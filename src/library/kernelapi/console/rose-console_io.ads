6af0480af77a3caf573261f336d38f7b5334486b
Fraser Wilson <blancolioni@gmail.com>
2018-12-13 11:27:38 +0100

    Rose.Console_IO: added vesions of Put for Object_Id and Storage_Array kernel: removed some logging

--- src/library/kernelapi/console/rose-console_io.ads
+++ src/library/kernelapi/console/rose-console_io.ads
@@ -1,4 +1,7 @@
+with System.Storage_Elements;
+
 with Rose.Capabilities;
+with Rose.Objects;
 with Rose.Words;
 
 package Rose.Console_IO is
@@ -17,6 +20,8 @@ package Rose.Console_IO is
    procedure Put (Item : Rose.Words.Word_16);
    procedure Put (Item : Rose.Words.Word_32);
    procedure Put (Item : Rose.Words.Word_64);
+   procedure Put (Item : Rose.Objects.Object_Id);
+   procedure Put (Item : System.Storage_Elements.Storage_Array);
    procedure Put_Line (Text : String);
    procedure New_Line;
 
