1621958d220b279fcb38b3064916cbc02b06c21c
Fraser Wilson <blancolioni@gmail.com>
2018-12-17 05:47:02 +0100

    init: supply a region and a standard output to exec

--- src/servers/init/src/init-installer.ads
+++ src/servers/init/src/init-installer.ads
@@ -3,13 +3,14 @@ with Rose.Words;
 package Init.Installer is
 
    procedure Install_Exec_Library
-     (Create_Cap    : Rose.Capabilities.Capability;
-      Storage_Cap   : Rose.Capabilities.Capability;
-      Reserve_Cap   : Rose.Capabilities.Capability;
-      Launch_Cap    : Rose.Capabilities.Capability;
-      Cap_Stream    : Rose.Capabilities.Capability;
-      Binary_Stream : Rose.Capabilities.Capability;
-      Binary_Length : Rose.Words.Word);
+     (Create_Cap      : Rose.Capabilities.Capability;
+      Storage_Cap     : Rose.Capabilities.Capability;
+      Reserve_Cap     : Rose.Capabilities.Capability;
+      Launch_Cap      : Rose.Capabilities.Capability;
+      Cap_Stream      : Rose.Capabilities.Capability;
+      Standard_Output : Rose.Capabilities.Capability;
+      Binary_Stream   : Rose.Capabilities.Capability;
+      Binary_Length   : Rose.Words.Word);
 
    function Install_Executable
      (Create_Cap    : Rose.Capabilities.Capability;
