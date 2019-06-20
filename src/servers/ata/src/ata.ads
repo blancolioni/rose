6ce0cfe58cc6fb69103a41135c7fa3ed4eb74144
Fraser Wilson <blancolioni@gmail.com>
2019-06-20 08:21:08 +0200

    ata: reserve secondary drive interrupt

--- src/servers/ata/src/ata.ads
+++ src/servers/ata/src/ata.ads
@@ -7,20 +7,21 @@ package ATA is
    Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 1;
    Console_Cap         : constant Rose.Capabilities.Capability := 2;
    PCI_Cap             : constant Rose.Capabilities.Capability := 3;
-   Reserve_IRQ_Cap     : constant Rose.Capabilities.Capability := 4;
+   Primary_IRQ_Cap     : constant Rose.Capabilities.Capability := 4;
+   Secondary_IRQ_Cap   : constant Rose.Capabilities.Capability := 5;
 
-   Command_0_Cap       : constant Rose.Capabilities.Capability := 5;
-   Control_0_Cap       : constant Rose.Capabilities.Capability := 6;
-   Data_0_Cap_8        : constant Rose.Capabilities.Capability := 7;
-   Data_0_Cap_Read_16  : constant Rose.Capabilities.Capability := 8;
-   Data_0_Cap_Write_16 : constant Rose.Capabilities.Capability := 9;
+   Command_0_Cap       : constant Rose.Capabilities.Capability := 6;
+   Control_0_Cap       : constant Rose.Capabilities.Capability := 7;
+   Data_0_Cap_8        : constant Rose.Capabilities.Capability := 8;
+   Data_0_Cap_Read_16  : constant Rose.Capabilities.Capability := 9;
+   Data_0_Cap_Write_16 : constant Rose.Capabilities.Capability := 10;
 
-   Command_1_Cap       : constant Rose.Capabilities.Capability := 10;
-   Control_1_Cap       : constant Rose.Capabilities.Capability := 11;
-   Data_1_Cap_8        : constant Rose.Capabilities.Capability := 12;
-   Data_1_Cap_Read_16  : constant Rose.Capabilities.Capability := 13;
-   Data_1_Cap_Write_16 : constant Rose.Capabilities.Capability := 14;
+   Command_1_Cap       : constant Rose.Capabilities.Capability := 11;
+   Control_1_Cap       : constant Rose.Capabilities.Capability := 12;
+   Data_1_Cap_8        : constant Rose.Capabilities.Capability := 13;
+   Data_1_Cap_Read_16  : constant Rose.Capabilities.Capability := 14;
+   Data_1_Cap_Write_16 : constant Rose.Capabilities.Capability := 15;
 
-   Set_Timeout_Cap     : constant Rose.Capabilities.Capability := 15;
+   Set_Timeout_Cap     : constant Rose.Capabilities.Capability := 16;
 
 end ATA;
