69773039306febe7610ae949e2afe8be6617c5e4
Fraser Wilson <53253609+blancolioni@users.noreply.github.com>
2019-06-23 20:45:12 +0200

    keyboard: re-ordered caps

--- src/drivers/keyboard/src/keyboard.ads
+++ src/drivers/keyboard/src/keyboard.ads
@@ -6,9 +6,9 @@ package Keyboard is
 
    Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 1;
    Register_IRQ_Cap    : constant Rose.Capabilities.Capability := 2;
-   Read_Key_Cap        : constant Rose.Capabilities.Capability := 3;
-   Read_Status_Cap     : constant Rose.Capabilities.Capability := 4;
-   Command_Cap         : constant Rose.Capabilities.Capability := 5;
-   Data_Cap            : constant Rose.Capabilities.Capability := 6;
+   Command_Cap         : constant Rose.Capabilities.Capability := 3;
+   Data_Cap            : constant Rose.Capabilities.Capability := 4;
+   Read_Key_Cap        : constant Rose.Capabilities.Capability := 5;
+   Read_Status_Cap     : constant Rose.Capabilities.Capability := 6;
 
 end Keyboard;
