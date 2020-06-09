4eb3e4b5ac46a625ad7d42980337b81486fcbc9c
Fraser Wilson <fraser@thewhitelion.eu>
2020-06-09 12:32:01 +0200

    Updates for gcc 10

--- rts/copy-runtime.sh
+++ rts/copy-runtime.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+GCCVERSION=10
 for f in "ada.ads" "a-unccon.ads" "a-uncdea.ads" "gnat.ads" "g-souinf.ads" \
    interfac.ads s-atacco.adb s-atacco.ads s-maccod.ads s-stoele.adb \
    s-stoele.ads i-c.ads s-addima.ads s-addope.ads s-imgint.ads \
@@ -6,7 +7,7 @@ for f in "ada.ads" "a-unccon.ads" "a-uncdea.ads" "gnat.ads" "g-souinf.ads" \
 do
     mkdir -p ./gnat-src
     rm -f ./gnat-src/$f
-    cp /usr/lib/gcc/x86_64-redhat-linux/9/adainclude/$f ./gnat-src
+    cp /usr/lib/gcc/x86_64-redhat-linux/$GCCVERSION/adainclude/$f ./gnat-src
     rm -f ./build/i686/adainclude/$f
     ln -s `pwd`/gnat-src/$f `pwd`/build/i686/adainclude/$f
 done
