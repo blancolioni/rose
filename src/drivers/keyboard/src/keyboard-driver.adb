8ec5058a434e38d7173864c3371974280a0c7f1b
Fraser Wilson <fraser@thewhitelion.eu>
2021-01-12 22:55:08 +0100

    keyboard: remove startup message

--- src/drivers/keyboard/src/keyboard-driver.adb
+++ src/drivers/keyboard/src/keyboard-driver.adb
@@ -5,7 +5,6 @@ with Keyboard.Server;
 procedure Keyboard.Driver is
 begin
    Rose.Console_IO.Open (Console_Cap);
-   Rose.Console_IO.Put_Line ("keyboard: starting");
    Keyboard.Server.Create_Server;
    Keyboard.Server.Start_Server;
 end Keyboard.Driver;
