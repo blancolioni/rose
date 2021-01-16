34eb7b594d8ce0043cfd788b4742a662fec21665
Fraser Wilson <fraser@thewhitelion.eu>
2021-01-16 18:32:55 +0100

    rts: updated standard caps

--- src/rts/src/adainclude/s-stacap.ads
+++ src/rts/src/adainclude/s-stacap.ads
@@ -3,8 +3,9 @@ with Rose.Capabilities;
 private package System.Standard_Caps is
 
    Exit_Cap              : constant Rose.Capabilities.Capability := 1;
-   Create_Endpoint_Cap   : constant Rose.Capabilities.Capability := 2;
-   Cap_Set_Constructor   : constant Rose.Capabilities.Capability := 3;
-   Argument_Cap_Set      : constant Rose.Capabilities.Capability := 4;
+   Heap_Cap              : constant Rose.Capabilities.Capability := 2;
+   Create_Endpoint_Cap   : constant Rose.Capabilities.Capability := 3;
+   Cap_Set_Constructor   : constant Rose.Capabilities.Capability := 4;
+   Argument_Cap_Set      : constant Rose.Capabilities.Capability := 5;
 
 end System.Standard_Caps;
