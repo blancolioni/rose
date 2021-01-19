cf66374d92a9796cd4f82c6315765474167aec5b
Fraser Wilson <fraser@thewhitelion.eu>
2021-01-19 13:13:53 +0100

    drivers/keyboard: updated standard caps, publish interface

--- src/drivers/keyboard/src/keyboard.ads
+++ src/drivers/keyboard/src/keyboard.ads
@@ -4,12 +4,12 @@ package Keyboard is
 
    pragma Pure (Keyboard);
 
-   Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 1;
-   Register_IRQ_Cap    : constant Rose.Capabilities.Capability := 2;
-   Command_Cap         : constant Rose.Capabilities.Capability := 3;
-   Data_Cap            : constant Rose.Capabilities.Capability := 4;
-   Read_Key_Cap        : constant Rose.Capabilities.Capability := 5;
-   Read_Status_Cap     : constant Rose.Capabilities.Capability := 6;
-   Console_Cap         : constant Rose.Capabilities.Capability := 7;
+   Create_Endpoint_Cap : constant Rose.Capabilities.Capability := 4;
+   Register_IRQ_Cap    : constant Rose.Capabilities.Capability := 5;
+   Command_Cap         : constant Rose.Capabilities.Capability := 6;
+   Data_Cap            : constant Rose.Capabilities.Capability := 7;
+   Read_Key_Cap        : constant Rose.Capabilities.Capability := 8;
+   Read_Status_Cap     : constant Rose.Capabilities.Capability := 9;
+   Console_Cap         : constant Rose.Capabilities.Capability := 10;
 
 end Keyboard;
