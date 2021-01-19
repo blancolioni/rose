46f7ed07a0eed2269c898fb438745fa8bf4e5d6c
Fraser Wilson <fraser@thewhitelion.eu>
2021-01-19 13:12:29 +0100

    drivers/exec: updated capability values

--- src/drivers/exec/src/exec.ads
+++ src/drivers/exec/src/exec.ads
@@ -4,13 +4,13 @@ package Exec is
 
    pragma Pure (Exec);
 
-   Create_Endpoint_Cap  : constant Rose.Capabilities.Capability := 3;
-   Delete_Endpoint_Cap  : constant Rose.Capabilities.Capability := 4;
-   Rescind_Endpoint_Cap : constant Rose.Capabilities.Capability := 5;
-   Console_Cap          : constant Rose.Capabilities.Capability := 6;
+   Create_Endpoint_Cap  : constant Rose.Capabilities.Capability := 4;
+   Delete_Endpoint_Cap  : constant Rose.Capabilities.Capability := 5;
+   Rescind_Endpoint_Cap : constant Rose.Capabilities.Capability := 6;
+   Console_Cap          : constant Rose.Capabilities.Capability := 7;
 
-   Create_Process_Cap   : constant Rose.Capabilities.Capability := 7;
-   Region_Cap           : constant Rose.Capabilities.Capability := 8;
-   Storage_Cap          : constant Rose.Capabilities.Capability := 9;
+   Create_Process_Cap   : constant Rose.Capabilities.Capability := 8;
+   Region_Cap           : constant Rose.Capabilities.Capability := 9;
+   Storage_Cap          : constant Rose.Capabilities.Capability := 10;
 
 end Exec;
