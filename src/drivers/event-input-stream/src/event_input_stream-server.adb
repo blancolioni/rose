42289038ed68d5f5621228d5dc29dfc3f305da0d
Fraser Wilson <fraser@thewhitelion.eu>
2021-09-10 14:53:41 +0200

    event-input-stream fixes

--- src/drivers/event-input-stream/src/event_input_stream-server.adb
+++ src/drivers/event-input-stream/src/event_input_stream-server.adb
@@ -7,8 +7,11 @@ with Rose.Invocation;
 with Rose.Interfaces.Event_Listener;
 with Rose.Interfaces.Stream_Reader;
 
+with Rose.System_Calls;
 with Rose.System_Calls.Server;
 with Rose.Server;
+
+with Rose.Console_IO;
 
 package body Event_Input_Stream.Server is
 
@@ -35,6 +38,10 @@ package body Event_Input_Stream.Server is
                           Rose.System_Calls.Server.Create_Endpoint
                             (Create_Endpoint_Cap,
                              Rose.Interfaces.Stream_Reader.Read_Endpoint);
+      On_Event_Cap    : constant Rose.Capabilities.Capability :=
+                          Rose.System_Calls.Server.Create_Endpoint
+                            (Create_Endpoint_Cap,
+                             Rose.Interfaces.Event_Listener.On_Event_Endpoint);
       Params          : aliased Rose.Invocation.Invocation_Record;
       Reply           : aliased Rose.Invocation.Invocation_Record;
       Blocked_Request : Boolean := False;
@@ -45,11 +52,14 @@ package body Event_Input_Stream.Server is
         (Create_Endpoint_Cap,
          Rose.Interfaces.Get_Interface_Endpoint);
 
-      Rose.System_Calls.Server.Create_Anonymous_Endpoint
-        (Create_Endpoint_Cap,
-         Rose.Interfaces.Event_Listener.On_Event_Endpoint);
+      Rose.Server.Publish_Interface (3, Interface_Cap);
 
-      Rose.Server.Publish_Interface (3, Event_Source_Cap);
+      Rose.System_Calls.Initialize_Send (Params, Event_Source_Cap);
+      Rose.System_Calls.Invoke_Capability (Params);
+
+      Rose.System_Calls.Initialize_Send (Params, Params.Caps (0));
+      Rose.System_Calls.Send_Cap (Params, On_Event_Cap);
+      Rose.System_Calls.Invoke_Capability (Params);
 
       loop
          Rose.System_Calls.Initialize_Receive (Params, Receive_Cap);
@@ -59,10 +69,14 @@ package body Event_Input_Stream.Server is
 
          case Params.Endpoint is
             when Rose.Interfaces.Get_Interface_Endpoint =>
+               Rose.System_Calls.Initialize_Reply (Reply, Params.Reply_Cap);
                Rose.System_Calls.Send_Cap (Reply, Interface_Cap);
+               Rose.System_Calls.Invoke_Capability (Reply);
 
             when Rose.Interfaces.Stream_Reader.Stream_Reader_Interface =>
+               Rose.System_Calls.Initialize_Reply (Reply, Params.Reply_Cap);
                Rose.System_Calls.Send_Cap (Reply, Read_Cap);
+               Rose.System_Calls.Invoke_Capability (Reply);
 
             when Rose.Interfaces.Event_Listener.On_Event_Endpoint =>
                declare
@@ -80,8 +94,8 @@ package body Event_Input_Stream.Server is
                if Blocked_Request then
                   Rose.System_Calls.Initialize_Reply (Reply, Waiting_Request);
                   Rose.System_Calls.Send_Storage_Array
-                    (Params, Input_Buffer (1 .. Input_Last), False);
-                  Rose.System_Calls.Invoke_Capability (Params);
+                    (Reply, Input_Buffer (1 .. Input_Last), False);
+                  Rose.System_Calls.Invoke_Capability (Reply);
                   Input_Last := 0;
                   Blocked_Request := False;
                end if;
@@ -94,8 +108,8 @@ package body Event_Input_Stream.Server is
                      Rose.System_Calls.Initialize_Reply
                        (Reply, Params.Reply_Cap);
                      Rose.System_Calls.Send_Storage_Array
-                       (Params, Input_Buffer (1 .. Input_Last), False);
-                     Rose.System_Calls.Invoke_Capability (Params);
+                       (Reply, Input_Buffer (1 .. Input_Last), False);
+                     Rose.System_Calls.Invoke_Capability (Reply);
                      Input_Last := 0;
                   else
                      Blocked_Request := True;
@@ -104,7 +118,9 @@ package body Event_Input_Stream.Server is
                end;
 
             when others =>
-               null;
+               Rose.Console_IO.Put ("stream: invalid endpoint: ");
+               Rose.Console_IO.Put (Rose.Words.Word_64 (Params.Endpoint));
+               Rose.Console_IO.New_Line;
 
          end case;
 
