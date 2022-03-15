908029a08c8c1a2c7f86e5b157695dfc4102d92f
Fraser Wilson <fraser@thewhitelion.eu>
2022-03-15 10:20:32 +0100

    Remove generated files

--- src/library/kernelapi/generated/rose-interfaces-launch.ads
+++
@@ -1,9 +0,0 @@
-with Rose.Objects;
-
-package Rose.Interfaces.Launch is
-
-   Launch_Interface : constant Rose.Objects.Endpoint_Id :=
-      16#60A8_2B69_97DC#;
-   Launch_Endpoint : constant Rose.Objects.Endpoint_Id := 16#0377_796D_7E54#;
-
-end Rose.Interfaces.Launch;
--- src/library/kernelapi/generated/rose-interfaces-stream_writer.ads
+++
@@ -1,9 +0,0 @@
-with Rose.Objects;
-
-package Rose.Interfaces.Stream_Writer is
-
-   Stream_Writer_Interface : constant Rose.Objects.Endpoint_Id :=
-      16#50CB_136A_60B4#;
-   Write_Endpoint : constant Rose.Objects.Endpoint_Id := 16#2844_384F_C1C5#;
-
-end Rose.Interfaces.Stream_Writer;
